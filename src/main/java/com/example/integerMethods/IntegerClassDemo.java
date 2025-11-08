e676af3d856f880e0314b6263c5a8cb9bc4752d6
Swapnil Bokade <sbokade181@gmail.com>
2025-11-08 07:55:28 +0530

    added java integer class with all methods and correction

--- src/main/java/com/example/integerMethods/IntegerClassDemo.java
+++ src/main/java/com/example/integerMethods/IntegerClassDemo.java
@@ -1,6 +1,7 @@
 package com.example.integerMethods;
 
 public class IntegerClassDemo {
+
     public static void main(String[] args) {
         System.out.println("===== 1️⃣ Conversion Methods =====");
 
