89e6913fc922b1ff613fa6f8702b1c655d808caa
Swapnil Bokade <sbokade181@gmail.com>
2025-12-08 07:43:20 +0530

    cg8

--- src/main/java/com/example/InterviewProgram.java
+++ src/main/java/com/example/InterviewProgram.java
@@ -14,7 +14,6 @@ public class InterviewProgram {
             map.put(list.get(i), map.getOrDefault(list.get(i),0)+1);
         }
 
-
         for(Map.Entry<Integer,Integer> m: map.entrySet()){
             if(m.getValue()>1){
                 System.out.println(m.getKey()+" : "+m.getValue());
