0b0d0645097eda100eebb5f45657982ba27446c9
Swapnil Bokade <sbokade181@gmail.com>
2025-12-23 21:19:50 +0530

    tests

--- src/main/java/com/example/streamapi/TerminalStreamOperations.java
+++ src/main/java/com/example/streamapi/TerminalStreamOperations.java
@@ -8,6 +8,7 @@ public class TerminalStreamOperations {
     public static void main(String[] args) {
 
 
+
         List<Integer> numbers = List.of(3, 7, 2, 9, 5, 2, 7, 10, 4);
 
         System.out.println("Original List: " + numbers);
