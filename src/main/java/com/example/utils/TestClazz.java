a21aed368e3d6a1bbb68a11bcaac738ca4e35c01
Swapnil Bokade <sbokade181@gmail.com>
2025-12-27 13:44:01 +0530

    tests

---
+++ src/main/java/com/example/utils/TestClazz.java
@@ -0,0 +1,26 @@
+package com.example.utils;
+
+import  java.util.*;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+
+public class TestClazz {
+
+    public static  void main(String[] args) {
+        System.out.println("TestClazz is running...");
+
+        String str = "management";
+
+
+        Map<Character, Integer> countFreq = new LinkedHashMap<>();
+        for(char c : str.toCharArray()){
+            countFreq.put(c, countFreq.getOrDefault(c,0)+1);
+        }
+
+
+       countFreq.entrySet().stream().forEach(e-> System.out.println(e.getKey() + " : " + e.getValue()));
+
+
+
+    }
+}
